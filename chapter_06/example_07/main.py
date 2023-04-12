2b118fc60cd962300b17e2c20c7edd503dd8f409
Christofer W <christofer.werner@tecnoandina.cl>
2023-04-11 20:28:31 -0400

    folder structure changed to chapter* format

--- chapter_06/example_07/main.py
+++ chapter_06/example_07/main.py
@@ -1,4 +1,5 @@
 # Reinforcement Learning An Introduction Page 134. Double Learning
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,9 +7,7 @@ import time
 
 
 class Agent:
-
     def __init__(self, update_method="Double Q-Learning", b_card=10):
-
         # Parameters
         self.epsilon = 0.1
         self.alpha = 0.1
@@ -18,17 +17,19 @@ class Agent:
         self.update = update_method
 
         # Nodes
-        self.Terminal = {"Q1": np.zeros(1),
-                         "Q2": np.zeros(1)}
+        self.Terminal = {"Q1": np.zeros(1), "Q2": np.zeros(1)}
 
-        self.B = {"actions": {i: self.Terminal for i in range(b_card)},
-                  "Q1": np.zeros(b_card),
-                  "Q2": np.zeros(b_card)}
+        self.B = {
+            "actions": {i: self.Terminal for i in range(b_card)},
+            "Q1": np.zeros(b_card),
+            "Q2": np.zeros(b_card),
+        }
 
-        self.A = {"actions": {0: self.Terminal,
-                              1: self.B},
-                  "Q1": np.zeros(2),
-                  "Q2": np.zeros(2)}
+        self.A = {
+            "actions": {0: self.Terminal, 1: self.B},
+            "Q1": np.zeros(2),
+            "Q2": np.zeros(2),
+        }
 
         # Initial State. The States are 0:A, 1:B
         self.state = self.A
@@ -70,23 +71,27 @@ class Agent:
         return argmax, state["actions"][argmax]
 
     def q_learning_update(self, initial_state, action, end_state):
-        initial_state["Q1"][action] = initial_state["Q1"][action] + self.alpha * (self.reward() + max(end_state["Q1"]) -
-                                                                                  initial_state["Q1"][action])
+        initial_state["Q1"][action] = initial_state["Q1"][action] + self.alpha * (
+            self.reward() + max(end_state["Q1"]) - initial_state["Q1"][action]
+        )
 
     def double_q_learning_update(self, initial_state, action, end_state):
         if np.random.random() < 0.5:
-            argmax = np.random.choice(np.flatnonzero(end_state["Q2"] == end_state["Q2"].max()))
+            argmax = np.random.choice(
+                np.flatnonzero(end_state["Q2"] == end_state["Q2"].max())
+            )
             initial_state["Q1"][action] = initial_state["Q1"][action] + self.alpha * (
-                    self.reward() + end_state["Q1"][argmax] -
-                    initial_state["Q1"][action])
+                self.reward() + end_state["Q1"][argmax] - initial_state["Q1"][action]
+            )
         else:
-            argmax = np.random.choice(np.flatnonzero(end_state["Q1"] == end_state["Q1"].max()))
+            argmax = np.random.choice(
+                np.flatnonzero(end_state["Q1"] == end_state["Q1"].max())
+            )
             initial_state["Q2"][action] = initial_state["Q2"][action] + self.alpha * (
-                    self.reward() + end_state["Q2"][argmax] -
-                    initial_state["Q2"][action])
+                self.reward() + end_state["Q2"][argmax] - initial_state["Q2"][action]
+            )
 
     def time_step(self):
-
         action, next_state = self.choose_action(self.state)
 
         if self.update == "Double Q-Learning":
@@ -101,7 +106,6 @@ class Agent:
         return previous_state, action
 
     def episode(self):
-
         while self.state != self.Terminal:
             state, action = self.time_step()
         self.state = self.A
@@ -109,7 +113,6 @@ class Agent:
         return state, action
 
     def cycle(self, episodes=300):
-
         list_actions = []
         for ep in range(episodes):
             left_picked = 0
@@ -125,9 +128,7 @@ class Agent:
         return list_actions
 
     def run(self, runs=10000):
-
         for run in range(1, runs + 1):
-
             self.restart()
 
             left_picked_run = self.cycle()
@@ -142,7 +143,6 @@ class Agent:
 
 
 if __name__ == "__main__":
-
     t1 = time.time()
     DQL = Agent(update_method="Double Q-Learning")
     DQL.run(runs=10000)
